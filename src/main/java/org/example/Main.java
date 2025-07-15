f78ec6176e1457d78d4d166a1a57a7da229868b6
barisozcan93 <barisozcn93@gmail.com>
2025-07-16 00:17:10 +0300

    solution added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,35 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       /* Point first = new Point(6, 5);
+
+        Point second = new Point(3, 1);
+
+        System.out.println("distance(0,0)= " + first.distance());
+
+        System.out.println("distance(second)= " + first.distance(second));
+
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+
+        Point point = new Point(0,0);
+
+        System.out.println("distance()= " + point.distance());*/
+
+        Player player = new Player("emrah", 100, Weapon.AXE);
+        System.out.println("healthRemaining: " + player.healthRemaining());
+
+
+        Player player2 = new Player("hakan", 90, Weapon.SWORD);
+        System.out.println("healthRemaining: " + player.healthRemaining());
+
+        player.loseHealth(Integer.valueOf((int) (player2.getWeapon().getDamage() * player2.getWeapon().getAttackSpeed())));
+        System.out.println("healthremaining after damage: " + player.healthRemaining());
+
+        player.restoreHealth(70);
+        System.out.println("healthremaining after restore: " + player.healthRemaining());
+        player2.loseHealth((int) (player.getWeapon().getDamage() * player.getWeapon().getAttackSpeed()));
+        System.out.println("health remaining after attack for player 2: " + player2.healthRemaining());
+
+
     }
 }
---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
+
+    public double distance(Point p) {
+        if (p == null) {
+            System.out.println("point object cannot be null!!");
+            return 0;
+        }
+        return distance(p.x, p.y);
+    }
+
+    public double distance(int a, int b) {
+        return Math.sqrt(Math.pow(x - a, 2) + Math.pow(y - b, 2));
+    }
+}
